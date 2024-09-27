e6d4a69bcc5a0cc0a3f6a9f5d1054091467f6e84
ApurboShib <shibjoy088.gmail.com>
2024-09-27 15:59:56 +0600

    update

--- online_practice_prob/array.asm
+++ online_practice_prob/array.asm
@@ -1,7 +1,6 @@
+.DATA
+W DW 10, 20, 30, 40
 
-
-W DW 10,20,30,40
-
-MOV AX, W
-XCHG W+6, AX
-MOV W, AX o
+.CODE
+MOV AX, [W + 6]  ; Load the value at W+6 (which is the third element, 30) into AX
+MOV [W], AX      ; Move the value from AX (30) to W (the first element)
